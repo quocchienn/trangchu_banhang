fd69b9218ba16b59934622156431eae7026b181d
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2025-07-08 16:05:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -82,13 +82,21 @@ async function submitOrder(event) {
 
 function showBankQR(name) {
     const content = `MUA ${currentProduct} - ${name}`;
+
+    // Chuyển currentPrice từ '59.000₫' => 59000
+    let amount = currentPrice.replace(/[^\d]/g, '');
+
     document.getElementById('bank-transfer-content').innerText = content;
+
     const bank = 'MB';
-    const account = '701235';
-    const template = `https://img.vietqr.io/image/${bank}-${account}-compact2.png?amount=&addInfo=${encodeURIComponent(content)}&accountName=NGUYEN%20VAN%20A`;
+    const account = '123456789';
+
+    const template = `https://img.vietqr.io/image/${bank}-${account}-compact2.png?amount=${amount}&addInfo=${encodeURIComponent(content)}&accountName=NGUYEN%20VAN%20A`;
+
     document.getElementById('bank-qr-img').src = template;
     document.getElementById('bank-transfer-qr').style.display = 'block';
 }
+
 
 // Smooth scrolling cho navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
